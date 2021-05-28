3dfc2e8cd408b4bdf3a69ed8b9f9f786a73454a2
Marcel Arie <googlillo@gmail.com>
2021-05-28 19:40:11 +0200

    add: base endpoints

---
+++ src/controllers/object-controller.js
@@ -0,0 +1,10 @@
+import Object from '../models/Object-model.js'
+
+// ENDPOINTS
+//OBJECT:
+// 6. Create object: adds a new object to data source.
+// 7. Get object by id: returns the object for the given id.
+// 8. Upgrade object: increase/descrease the value of the object given by id with a new value
+// 9. Destroy object: remove an object from available objects
+// BONUS:
+// ...
---
+++ src/controllers/player-controller.js
@@ -0,0 +1,37 @@
+import Player from '../models/Player-model.js'
+
+// ENDPOINTS
+// PLAYER:
+// 1. List all players.
+// 2. Create player: adds a new player to data source.
+// 3. Get player by id: returns the player for the given id.
+// 4. Arm a player with an object in its bag.
+// 5. Kill a player: sets player health to 0.
+// BONUS:
+// 3. Implement pick up item endpoint: one player add to its bag one item that
+// doesn't belong to any other player.
+// 4. Implement attack player endpoint: one player attacks another player using
+// an object from its bag. Adjust health accordingly
+// 5. Implement steal bag from player endpoint: one player steals everything
+// from another player. Bag objects are moved from one player to another.
+// 6. Implement resurrect player endpoint: bring back to life a dead player
+// using its id.
+// 7. Implement use object endpoint: a player use an object against another
+// player or itself.
+// 8. Are you having fun? You are free to extend the game with new
+// functionality.
+
+export async function getAllPlayers(req, res) {
+}
+
+export async function postPlayer(req, res) {
+}
+
+export async function getPlayerById(req, res) {
+}
+
+export async function addObjectToPlayer(req, res) {
+}
+
+export async function deletePlayer(req,res) {
+}
