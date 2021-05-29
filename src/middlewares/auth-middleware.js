e27fc270778e6c0e1ebcab2b5a058d52346a74b9
Marcel Arie <googlillo@gmail.com>
2021-05-29 16:28:16 +0200

    build: jwt auth-middleware

---
+++ src/middlewares/auth-middleware.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken'
+
+
+async function authMiddleware(req, res, next) {
+    const authHeader = req.headers.authorization
+
+    if (authHeader) {
+        const token = authHeader.substring(7)
+        console.log(token)
+
+        jwt.verify(token, process.env.SECRET, (err, user) => {
+            if (err) {
+                return res
+                    .status(401)
+                    .send({ message: 'Token not authorized.' })
+            }
+
+            // still don't understand this part
+            req.user = user
+            next()
+        })
+    } else {
+        res.status(401).send({ message: 'Token not authorized.' })
+    }
+}
+
+export default authMiddleware
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 
+import authMiddleware from './middlewares/auth-middleware.js'
 import { ObjectRouter, PlayerRouter, UserRouter } from './routes/index.js'
 const app = express()
 
@@ -10,8 +11,9 @@ app.use(express.json())
 app.use(cors())
 // app.disable("x-powered-by"); // QUESTION: any reason is this line here?
 
+app.use('/user', UserRouter)
+app.use(authMiddleware)
 app.use('/player', PlayerRouter)
 app.use('/object', ObjectRouter)
-app.use('/user', UserRouter)
 
 export default app
