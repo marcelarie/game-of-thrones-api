4e0a000f91e6d85235834e9514b2388126403e54
Marcel Arie <googlillo@gmail.com>
2021-05-29 16:05:04 +0200

    build: basic login endpoint

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,4 +1,5 @@
 import UserRepo from '../models/User-model.js'
+import bcrypt from 'bcrypt'
 
 export async function singUp(req, res) {
     const { body } = req
@@ -6,6 +7,17 @@ export async function singUp(req, res) {
         const response = await UserRepo.create(body)
         if (!response) res.status(400).json(response)
         if (response) res.status(200).json(response)
+    } catch ({ message }) {
+        res.status(500).send({ message })
+    }
+}
+
+export async function login(req, res) {
+    const { body } = req
+    try {
+        const user = await UserRepo.findOne({ username: body.username })
+        const passwordCheck = await bcrypt.compare(body.password, user.password)
+        res.status(200).send({ message: 'Logged In' })
     } catch ({ message }) {
         res.status(500).send({ message })
     }
--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -4,5 +4,6 @@ import * as UserMethods from '../controllers/user-controller.js'
 const UserRouter = Router()
 
 UserRouter.post('/sign-up', UserMethods.singUp)
+UserRouter.post('/login', UserMethods.login)
 
 export default UserRouter
