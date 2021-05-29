fc50e9617ce67548b00c65687b5ac801f8fe6ab5
Marcel Arie <googlillo@gmail.com>
2021-05-29 16:14:19 +0200

    build: jwt token

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,5 +1,6 @@
 import UserRepo from '../models/User-model.js'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 export async function singUp(req, res) {
     const { body } = req
@@ -17,7 +18,18 @@ export async function login(req, res) {
     try {
         const user = await UserRepo.findOne({ username: body.username })
         const passwordCheck = await bcrypt.compare(body.password, user.password)
-        res.status(200).send({ message: 'Logged In' })
+
+        if (!passwordCheck)
+            res.status(401).send({
+                message: 'Username or password are incorrect',
+            })
+
+        const token = jwt.sign({ id: user._id }, process.env.SECRET, {
+            expiresIn: 86400,
+        })
+
+        res.cookie('token', token, { httpOnly: true })
+        if (passwordCheck) res.status(200).send({ message: 'Logged In' })
     } catch ({ message }) {
         res.status(500).send({ message })
     }
